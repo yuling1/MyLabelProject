2bdb5f41d078efbff550ceb1b618c2eb354bd9f5
yuling1 <1120405837@qq.com>
2019-10-08 23:05:05 +0800

    controller sent json and view get data by ajax

--- LabelMVCProject/LabelMVCProject/Controllers/HomeController.cs
+++ LabelMVCProject/LabelMVCProject/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LabelMVCProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -30,6 +31,36 @@ namespace LabelMVCProject.Controllers
         public ActionResult TaskDetail()
         {
             return View();
+        }
+
+        public ActionResult GetTasks()
+        {
+            var label1 = new Label() { Type= "checkbox", InnerHtml= "True", ClassID= "classID1" };
+            var label2 = new Label() { Type = "checkbox", InnerHtml = "False", ClassID = "classID2" };
+            var label3 = new Label() { Type = "text", InnerHtml = "Edit: ", ClassID = "classID3" };
+            var label4 = new Label() { Type = "text", InnerHtml = "Delete: ", ClassID = "classID4" };
+            var label5 = new Label() { Type = "checkbox", InnerHtml = "True", ClassID = "classID5" };
+            var label6 = new Label() { Type = "checkbox", InnerHtml = "False", ClassID = "classID6" };
+            var labels1 = new List<Label>();
+            labels1.Add(label1);
+            labels1.Add(label2);
+            var labels2 = new List<Label>();
+            labels2.Add(label3);
+            labels2.Add(label4);
+            var labels3 = new List<Label>();
+            labels3.Add(label5);
+            labels3.Add(label6);
+            var rule1 = new Rule() { RuleName = "Rule1", Patten = "", Labels = labels1 };
+            var rule2 = new Rule() { RuleName = "Rule1", Patten = "", Labels = labels2 };
+            var rule3 = new Rule() { RuleName = "Rule1", Patten = "", Labels = labels3 };
+            var rules = new List<Rule>();
+            rules.Add(rule1);
+            rules.Add(rule2);
+            rules.Add(rule3);
+            var task = new Task() { Num = 1, ID = 001, Name = "小冰诗歌", Content = "古佛洞的尽头是低矮的棚屋", Rules = rules };
+            var tasks = new List<Task>();
+            tasks.Add(task);
+            return Json(tasks, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult TaskHistoryDetail()
---
+++ LabelMVCProject/LabelMVCProject/Models/Task.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LabelMVCProject.Models
+{
+    public class Task
+    {
+        public int Num { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public List<Rule> Rules { get; set; }
+    }
+
+    public class Rule
+    {
+        public string RuleName { get; set; }
+        public string Patten { get; set; }
+        public List<Label> Labels { get; set; }
+    }
+
+    public class Label
+    {
+        public string Type { get; set; }
+        public string InnerHtml { get; set; }
+        public string ClassID { get; set; }
+    }
+}
